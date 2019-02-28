f58aa9a546efdaafea0712f6eec5776cf00a8f3d
RoninGAO <minkou.lee@gmail.com>
2019-02-28 19:27:43 +0800

    try basic method

---
+++ start.py
@@ -0,0 +1,20 @@
+import math
+# print("TEST")
+x = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+y = 0.1
+# print(x/y)
+# print("")
+# print("123")
+for i in x:
+    print(i/y)
+
+print("===========================")
+# round()....
+x = [1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5]
+
+for i in x:
+    print(round(i))
+
+print("===========================")
+x = 4.57
+print(math.floor(x))
